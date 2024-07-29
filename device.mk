015e0c47f06e41055994fea3f17d82879cb34dad
devhunter1 <anubhabkundu2@gmail.com>
2024-07-29 20:59:21 +0530

    encryption and decryption support

--- device.mk
+++ device.mk
@@ -21,3 +21,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+    
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Some useful binaries
+PRODUCT_PACKAGES += shrink bxhsed
